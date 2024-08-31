15271a8246cdd78145590e046f7c1c22983d1d85
Indravahini <145686434+Indravahini@users.noreply.github.com>
2024-08-31 23:37:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql');
@@ -9,16 +11,13 @@ const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 app.use(express.json());
-app.use(cors());
-
 const db = mysql.createConnection({
-  host: "sql12.freesqldatabase.com",
-  user: "sql12728169",
-  password: "Di1w9HpjQM",
-  database: "sql12728169",
-  port : 3306
-});
-
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
+  });
 
 db.connect(err => {
     if (err) {
